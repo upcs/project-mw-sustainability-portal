6d6aed2410a93be1f1f19a3c4ea8594321bb057f
ClintSizemore <sizemoreclint@gmail.com>
2026-02-10 10:49:41 -0800

    Made page change in index -> projects

--- mw_sustainability_portal/routes/projects.js
+++ mw_sustainability_portal/routes/projects.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET projects page page. */
 router.get('/', function(req, res, next) {
-  res.render('index') 
+  res.render('projects') 
 });
 
 module.exports = router;
