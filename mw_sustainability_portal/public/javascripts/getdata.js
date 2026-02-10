4a8cdd199f4733151185957992ecba608c864957
ClintSizemore <sizemoreclint@gmail.com>
2026-02-09 17:10:50 -0800

    Change HTML to be called index and added data collection.

---
+++ mw_sustainability_portal/public/javascripts/getdata.js
@@ -0,0 +1,18 @@
+function takeinfo() {
+    let user=document.getElementById("user").value;
+    let password=document.getElementById("pass").value;
+    if (user == "user") {
+        document.getElementById("check1").innerHTML="ok user";
+    }
+    else {
+        document.getElementById("check1").innerHTML="BAD USER";
+    }
+
+    if (password == "pass") {
+        document.getElementById("check2").innerHTML="ok pass";
+    }
+    else {
+        document.getElementById("check2").innerHTML="BAD PASS";
+    }
+}
+
