d84561a2a1a96f2281a545939b381a5e7a390ad6
Landon J. Harrison <harrisol27@up.edu>
2026-02-19 10:05:57 -0800

    Initial run

--- mw_sustainability_portal/routes/projects.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET projects page page. */
-router.get('/', function(req, res, next) {
-  res.render('projects') 
-});
-
-module.exports = router;
