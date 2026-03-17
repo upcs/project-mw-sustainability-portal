f5406eff23be59d431ca5d92aa52ac5b18e59f84
Megan Ou <ou27@up.edu>
2026-03-16 20:49:33 -0700

    dynamic projects page start

--- mw_sustainability_portal/routes/projects.js
+++ mw_sustainability_portal/routes/projects.js
@@ -18,13 +18,31 @@ router.get('/' , async(req, res) => {
 router.post('/', function(req, res, next) {
     console.log('Pulling data from DB');
 
-    dbms.dbquery( 'SELECT * FROM project_list;', function (err, results) {
+    dbms.dbquery( 'SELECT name, team, image_route, page_route, html_generated FROM projects_list;', function (err, results) {
         if (err) {
             res.send('Bad bad things happened');
-        } else {
-            res.render('projects', {records: results});
+        } else { 
+            //to display projects in 2 different rows, split the results into a set of 2
+            results_json = JSON.stringify(results);
+            results_array = JSON.parse(results);
+            
+            splitted = split_results(results_array, 2);
+
+            console.log(splitted)
+
+            res.render('projects', {records1: splitted[0], records2: splitted[1]});
         }
     });
 });
+
+function split_results(arr, size) {
+    const split = [];
+    for (let i = 0; i < arr.length; i += size) {
+        split.push(arr.slice(i, i + size));
+    }
+
+    return split;
+}
+
 
 module.exports = router;
