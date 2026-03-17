f885b2ad505cae9f3529e254e27b76a280069127
Nux28 <nuxoll28@up.edu>
2026-03-17 11:11:42 -0700

    test12

--- mw_sustainability_portal/app.js
+++ mw_sustainability_portal/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var projectsRouter = require('./routes/projects');
 var authRouter = require('./routes/authRoutes');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
 app.use('/authRoutes', authRouter);
+app.use('/login', loginRouter);
 
 app.post('/submit', (req, res) => {
     console.log(req.body); // Access parsed data
