fa88a85c7fa834c6295348d0f985a4d348ecea84
Megan Ou <ou27@up.edu>
2026-03-17 10:24:48 -0700

    DB debugging

--- mw_sustainability_portal/routes/projects.js
+++ mw_sustainability_portal/routes/projects.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var dbms = require("./dbms.js");
+const { concat } = require("async");
+var app = express();
 
 /* GET projects page page. */
 router.get('/' , async(req, res) => {
@@ -18,14 +20,14 @@ router.get('/' , async(req, res) => {
 router.post('/', function(req, res, next) {
     console.log('Pulling data from DB');
 
-    dbms.dbquery( 'SELECT name, team, image_route, page_route, html_generated FROM projects_list;', function (err, results) {
+    dbms.dbquery( 'SELECT name, team, image_route, page_route, html_generated FROM projects_list', function (err, results) {
         if (err) {
             res.send('Bad bad things happened');
         } else { 
             //to display projects in 2 different rows, split the results into a set of 2
             results_json = JSON.stringify(results);
             results_array = JSON.parse(results);
-            
+
             splitted = split_results(results_array, 2);
 
             console.log(splitted)
