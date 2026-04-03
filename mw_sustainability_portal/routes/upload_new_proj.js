b4871ef7ce3a9df030a0809e0feeee8ea18b688b
Landon J. Harrison <harrisol27@up.edu>
2026-04-02 23:24:25 -0700

    Update upload_new_proj.js

--- mw_sustainability_portal/routes/upload_new_proj.js
+++ mw_sustainability_portal/routes/upload_new_proj.js
@@ -49,7 +49,7 @@ router.post('/', upload_limit, async (req, res) => {
     
     //change sql prompt in next spring, using id 18 as hardcoded value 
     const sql = "INSERT INTO `projects_list`( `name`, `team`, `image_route`) VALUES ('" + name + "','" + team + "', '/images/1600px_COLOURBOX9214366-3078337225.jpg');"+
-        "INSERT INTO 'project_assets`(`project_id`,`asset_route`, `is_text`) VALUES ((SELECT id FROM `project_list` WHERE name = '"+ name+"'), '"+descript_path+"', 1)";
+        "INSERT INTO 'project_assets`(`project_id`,`asset_route`, `is_text`) VALUES (project_id, '"+descript_path+"', 1) WHERE project_id > (SELECT id FROM `project_list` WHERE name = '"+ name+"')";
 
     dbms.dbquery(sql, (err, results) => {
         if (err){
