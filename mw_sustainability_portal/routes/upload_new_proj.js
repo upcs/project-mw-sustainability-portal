3a9017767cf27f464bd53a01e8641837e250a47a
Landon J. Harrison <harrisol27@up.edu>
2026-04-03 00:12:40 -0700

    Updating comments and cleaning up upload_new_proj
    The newest version of upload_new_proj allows the user to add a description to their new project request. Beyond adding the new project to the DB it also adds a new description file to the server file system for the project and uploads the route to that file to the assets database list. All previous commits that did not have specific messages was because I had to work on my laptop, commit, then pull to the VM for testing. Next step: Integrate the new project creation process into Megan's project templating page.

--- mw_sustainability_portal/routes/upload_new_proj.js
+++ mw_sustainability_portal/routes/upload_new_proj.js
@@ -1,31 +1,30 @@
-/* Landon Harrison Version 0318 */
+/* Landon Harrison Version 0403 */
 /* this js file is the direct contact for any js wishing to upload to the database and dbms.js */
-/* uses multer, posts, and the creation of an sql prompt to upload files -- use of github ai */
+/* uses multer, posts, and the creation of an sql prompt to upload a new project and file routes -- use of github ai */
 
-const express = require('express');
-var multer = require('multer');
-var path = require('path');
-var fs = require('fs/promises');
-const rate_limit = require('express-rate-limit');
-const dbms = require("./dbms");
+const express = require('express'); //router
+var multer = require('multer'); //file creation 
+var path = require('path'); //file pathing
+var fs = require('fs/promises'); //making directory
+const rate_limit = require('express-rate-limit'); //setting a limit to the amount of DB uploads
+const dbms = require("./dbms"); //database file
 const router = express.Router();
-const santizer = require('sanitize-filename');
+const santizer = require('sanitize-filename'); //sanitizing input 
 
 
 const upload_limit = rate_limit({
     windowMs: 15*60*1000, //15 minutes
     max: 40, //max upload rate per 15 minutes
 });
-
 router.use(upload_limit);
+
 
 /* creating the endpoint (name and path ) for the file */
 router.post('/', upload_limit, async (req, res) => {  
     
-    console.log('entered upload post');
 
-    const raw_name = req.body.name;
-    let name = santizer(raw_name || "");
+    const raw_name = req.body.name; //name without sanitize
+    let name = santizer(raw_name || ""); //sanitizing
     let team = req.body.team;
     let descript = req.body.description;
     console.log("New project upload: ", name, team, descript);
@@ -38,20 +37,21 @@ router.post('/', upload_limit, async (req, res) => {
     let descript_path = path.join(uploadDir, "description.txt");
     await fs.writeFile(descript_path, descript, "utf8");
 
-    /* not including "public" which conflicts with render_project */
+    /* not including "public" in path because it conflicts with render_project */
     uploadDir = path.join( "assets", name);
     descript_path = path.join(uploadDir, "description.txt");
 
-    console.log("New project upload: ", name, team);
-    // while(!file){
-      //   return res.status(400).send({message: "please select an image file"});
-    // }
-
-    //creating filename and filepath 
     
-    //change sql prompt in next spring, using id 18 as hardcoded value 
-    const sql = "INSERT INTO `projects_list`( `name`, `team`, `image_route`) VALUES ('" + name + "','" + team + "', '/images/1600px_COLOURBOX9214366-3078337225.jpg'); "+
-       "INSERT INTO `project_assets` (`project_id`, `asset_route`, `is_text`) SELECT id, '"+descript_path+"', 1  FROM `projects_list` WHERE name = '"+name+"'";
+    //sql prompt that uploads new project with name and team, image route hardcoded 
+    //also uploads route to desription file to assets 
+    const sql = "INSERT INTO `projects_list`( `name`, `team`, `image_route`)"+ 
+                    "VALUES ('" + name + "','" + team + "', '/images/1600px_COLOURBOX9214366-3078337225.jpg');"
+                    
+                    + "INSERT INTO `project_assets` (`project_id`, `asset_route`, `is_text`)" +
+                            "SELECT id, '"+descript_path+"', 1"  
+                            +   "FROM `projects_list`"
+                                +   "WHERE name = '"+name+"'"; 
+            
 
     console.log(sql);
 
